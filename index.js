0cb94c18114f83391958b4fdf3874cd719b63b73
ky5burke <ky5burke@gmail.com>
2024-03-01 22:14:39 -0600

    post request in js using fetch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+
+    .then(response => response.json())
+    .then(json => {
+        const newId = json.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = newId;
+        document.body.append(idElement);
+    })
+
+    .catch(error => {
+        const errorMessage = error.message;
+        document.body.append(errorMessage);
+    })
+};
+//function needs to return fetch()
